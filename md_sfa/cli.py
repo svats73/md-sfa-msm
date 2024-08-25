873c78492c2f46a341741d163a91787cfe01b488
svats73 <shrayvats@gmail.com>
2024-08-24 20:19:59 -0400

    added new command

--- md_sfa/cli.py
+++ md_sfa/cli.py
@@ -95,5 +95,14 @@ def dump_sfa_components(save_file):
     processor_instance.dump_sfa_components(save_file)
     save_processor_instance(processor_instance)
 
+@cli.command()
+@click.option('--dat_file', type=click.Path(exists=True))
+@click.option('--pdb_input', type=click.Path(exists=True))
+@click.option('--pdb_output', type=click.Path())
+def plumed_bfactor(dat_file, pdb_input, pdb_output):
+    """Calculate B-factors using PLUMED input and PDB files."""
+    processor = TrajProcessor()
+    processor.process_bfactor(dat_file, pdb_input, pdb_output)
+
 if __name__ == '__main__':
     cli()
